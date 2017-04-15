d09428cef561cca60e798f0a13fa8709fa514d78
Benoit ROBAGLIA <benoitrobaglia@gmail.com>
2017-04-15 14:53:42 +0200

    tre

--- script.js
+++ script.js
@@ -174,3 +174,20 @@ Highcharts.chart('container', {
     }
     ]
 });
+
+
+//Création des onglets pour la partie 1//
+
+function change_onglet(name)
+                {
+                        document.getElementById('onglet_'+anc_onglet).className = 'onglet_0 onglet';
+                        document.getElementById('onglet_'+name).className = 'onglet_1 onglet';
+                        document.getElementById('contenu_onglet_'+anc_onglet).style.display = 'none';
+                        document.getElementById('contenu_onglet_'+name).style.display = 'block';
+                        anc_onglet = name;
+                }
+
+var anc_onglet = 'figaro';
+                change_onglet(anc_onglet);
+
+
